c92a0d74e42b4d2741429618732eea73ca0c8630
oguzadali <ouzadali1@gmail.com>
2020-10-29 21:16:03 +0300

    index html created

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
