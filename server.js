cdccf98b45d7239df2994cfa9f4b9f801e1c5c74
oguzadali <ouzadali1@gmail.com>
2020-10-29 23:56:23 +0300

    favicon added/index html path edited with path

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+var favicon = require('serve-favicon');
+
+
 const port = 3000
 
 app.use(express.static('public'))
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.get('/', function (req, res) {
-    res.sendFile('E:/masaüstü/git-test/index.html');
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(port, () => {
